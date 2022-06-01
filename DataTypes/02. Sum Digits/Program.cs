5410f2ac822dc64c8e2c0d4513de7e3070317c8c
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-01 11:29:35 +0300

    Add files via upload

---
+++ DataTypes/02. Sum Digits/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02._Sum_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int singleDigit = 0;
+            int sum = 0;
+            for (int i = num; i > 0; i/=10)
+            {
+                singleDigit = i % 10;
+                sum += singleDigit;
+                
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
