5b8b4e97f36e3031953bb620987beaa2096450e1
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-01 11:30:20 +0300

    Add files via upload

---
+++ DataTypes/07. Water Overflow/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _07._Water_Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int Capacy= 255;
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= number; i++)
+            {
+                int litersAdded = int.Parse(Console.ReadLine());
+                sum += litersAdded;
+                if (sum>Capacy)
+                {
+                    sum -= litersAdded;
+                    Console.WriteLine("Insufficient capacity!");
+                }
+                
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
