ca99e0cbf30d8520c3e34acef83cb05349bf6b54
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-03 22:09:24 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Arrays/P03. Rounding Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace P03._Rounding_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] rawInput = Console.ReadLine().Split();
+            double[] numbers = new double[rawInput.Length];
+            for (int i = 0; i < rawInput.Length; i++)
+            {
+                numbers[i] = double.Parse(rawInput[i]);
+                
+            }
+            for (int i = 0; i < rawInput.Length; i++)
+            {
+                Console.WriteLine($"{numbers[i]} => {Math.Round(numbers[i], MidpointRounding.AwayFromZero)}");
+            }
+        }
+    }
+}
