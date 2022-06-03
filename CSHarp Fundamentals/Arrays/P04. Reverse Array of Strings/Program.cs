d53957a062f988980ae9508808ad87e3551b6fc7
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-03 22:11:09 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Arrays/P04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace P04._Reverse_Array_of_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] rawInput = Console.ReadLine().Split();
+            Array.Reverse(rawInput);
+            Console.WriteLine(string.Join(" ",rawInput));
+        }
+    }
+}
