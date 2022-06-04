fa4fbce2912a806f914165aa604d9d23b4ec3530
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-04 22:41:39 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Arrays/P06. Even and Odd Subtraction/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P06._Even_and_Odd_Subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            double sum1 = 0;
+            double sum2 = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                double numbers = double.Parse(input[i]);
+                if (numbers % 2 == 0)
+                {
+                    sum1 += numbers;
+                }
+                else
+                {
+                    sum2 += numbers;
+                }
+            }
+            Console.WriteLine(sum1-sum2);
+        }
+    }
+}
