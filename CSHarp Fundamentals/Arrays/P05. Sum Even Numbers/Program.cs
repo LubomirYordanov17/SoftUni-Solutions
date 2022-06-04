6db8788ceea99ab61b849c8d82bd9246e981021c
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-04 22:41:17 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Arrays/P05. Sum Even Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace P05._Sum_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            double sum = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                double numbers= double.Parse(input[i]);
+                if (numbers%2==0)
+                {
+                    sum += numbers;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
