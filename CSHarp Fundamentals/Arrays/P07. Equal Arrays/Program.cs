dbfc77cecf642a8e61f8700ef04ab1778af1f6f3
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-04 22:43:35 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Arrays/P07. Equal Arrays/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace P07._Equal_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            string[] input2 = Console.ReadLine().Split();
+            double sum1 = 0;
+            double WrongIndex = -1;
+           
+            for (int i = 0; i < input.Length; i++)
+            {
+                double numbers = double.Parse(input[i]);
+                double numbers2 = double.Parse(input2[i]);
+                sum1 += numbers;
+
+                if (numbers!=numbers2)
+                {
+                    WrongIndex = i;
+                    break;
+                }
+
+            }
+            if (WrongIndex==-1)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum1}");
+            }
+            else
+            {
+                Console.WriteLine($"Arrays are not identical. Found difference at {WrongIndex} index");
+            }
+        }
+    }
+}
