ac6cb1f5a448008886dc24785cf00997a0fd9e05
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-04 22:43:58 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Arrays/P08. Condense Array to Number/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace P08._Condense_Array_to_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = int.Parse(Console.ReadLine().Split(' '));
+                
+        }
+    }
+}
