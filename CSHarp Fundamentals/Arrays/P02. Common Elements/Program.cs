18bdcfb428f3fe93887f5da8166702fa6916d925
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-05 17:17:23 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Arrays/P02. Common Elements/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P02._Common_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstRow=Console.ReadLine().Split(' ');
+            string[] secondRow=Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < secondRow.Length; i++)
+            {
+                string secondIn = secondRow[i];
+                for (int j = 0; j < firstRow.Length; j++)
+                {
+                    string firstIn = firstRow[j];
+                    if (secondIn==firstIn)
+                    {
+                        Console.Write($"{secondIn} ");
+                    }
+                }
+            }
+
+        }
+    }
+}
