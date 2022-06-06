ecf05ff0b1b82f00cbca7bbc244d6047055c8ce4
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-06 12:39:02 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Arrays/P08. Magic Sum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+namespace P08._Magic_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+           
+            int currSum = int.Parse(Console.ReadLine());
+            string result=string.Empty;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j=i+1; j < arr.Length; j++)
+                {
+                    if (arr[i]+arr[j]==currSum)
+                    {
+                        result += $"{arr[i]} {arr[j]}\n";
+                        
+                    }
+                }
+                
+            }
+            Console.Write(result);
+        }
+    }
+}
