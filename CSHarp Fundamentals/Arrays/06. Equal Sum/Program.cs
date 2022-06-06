ad81a3299b0c4b09d11076817c12bb149ac26f4a
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-06 12:36:18 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Arrays/06. Equal Sum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+namespace _06._Equal_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToArray();
+            int leftSum = 0;
+            int rightSum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    rightSum += arr[j];
+                }
+                if (i > 0)
+                {
+                    for (int y = i - 1; y >= 0; y--)
+                    {
+                        leftSum += arr[y];
+                    }
+                }
+                
+                if (rightSum==leftSum)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+                else
+                {
+                    if (i==(arr.Length-1))
+                    {
+                        Console.WriteLine("no");
+                        break;
+                    }
+                    leftSum = 0;
+                    rightSum = 0;
+                }
+            }   
+        }
+    }
+}
---
+++ CSHarp Fundamentals/Arrays/P04. Array Rotation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+namespace P04._Array_Rotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr=Console.ReadLine()
+                .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                int firstEl = arr[0];
+                for (int j = 0; j < arr.Length-1; j++)
+                {
+                    arr[j] = arr[j + 1];
+                }
+                arr[arr.Length-1] = firstEl;
+            }
+            foreach (var i in arr)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+    }
+}
