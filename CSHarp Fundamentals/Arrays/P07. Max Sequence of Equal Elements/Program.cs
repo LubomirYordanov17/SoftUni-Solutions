3a9fab59fb9446843a1b1c14d2faeff3eda356bf
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-06 12:37:16 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Arrays/P07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+namespace P07._Max_Sequence_of_Equal_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int count = 0;
+            int biggestCount = 0;
+            string currResult = string.Empty;
+            string result = string.Empty;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] != arr[j])
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        count++;
+                        currResult = currResult + $"{arr[j]} ";
+                    }
+                    if (biggestCount < count)
+                    {
+                        biggestCount = count;
+                        result = currResult+$"{arr[j]}";
+                    }
+
+                }
+                currResult = string.Empty;
+                count = 0;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
