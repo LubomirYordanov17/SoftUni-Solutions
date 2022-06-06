c075e86d42e03fc6b974258533173ba7d7cf775f
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-06 12:37:03 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Arrays/P05. Top Integers/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+namespace P05._Top_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+               .Split() 
+               .Select(int.Parse)
+               .ToArray();
+            int[] topInteger=new int[arr.Length];
+            int topIntegerIndex = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int currNum = arr[i];
+                bool isTopInteger = true;
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    int nextNum=arr[j];
+                    if (currNum <= nextNum)
+                    {
+                        isTopInteger = false;
+                        break;
+                    }
+                    
+                    
+                }
+                if (isTopInteger == true)
+                {
+                    topInteger[topIntegerIndex] = currNum;
+                    topIntegerIndex++;
+                }
+
+            }
+            for (int i = 0; i < topIntegerIndex; i++)
+            {
+                int currTopinteger = topInteger[i];
+                Console.Write($"{currTopinteger} ");
+            }
+        }
+    }
+}
