ddfb99cb5da97ec404f8effe867d8e2909de8c7a
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-07 21:05:15 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Methods/02. Grades/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _02._Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Grades();
+            
+        }
+        static void Grades()
+        {
+            double a = double.Parse(Console.ReadLine());
+            if (a < 3)
+            {
+                Console.WriteLine("Fail");
+            }
+            else if (a<3.50)
+            {
+                Console.WriteLine("Poor");
+            }
+            else if (a<4.50)
+            {
+                Console.WriteLine("Good");
+            }
+            else if (a < 5.50)
+            {
+                Console.WriteLine("Very good");
+
+            }else if (a < 6)
+            {
+                Console.WriteLine("Excellent");
+            }
+        }
+    }
+}
