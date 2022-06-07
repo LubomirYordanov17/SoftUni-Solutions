1e9028160283c6d9c4b9934daf306c249b14bc29
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-07 21:04:05 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Methods/01. Sign of Integer Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01._Sign_of_Integer_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            isPositive();
+        }
+        static void isPositive()
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (a>0)
+            {
+                Console.WriteLine($"The number {a} is positive. ");
+            }
+            else if(a==0)
+            {
+                Console.WriteLine($"The number {a} is zero. ");
+            }
+            else if (a<0)
+            {
+                Console.WriteLine($"The number {a} is negative. ");
+            }
+        }
+    }
+}
