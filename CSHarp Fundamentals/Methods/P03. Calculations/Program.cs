4f49f695d38bcf5c92b317fb53e70e9d67a5521e
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-08 18:38:04 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Methods/P03. Calculations/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace P03._Calculations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator();
+        }
+        static void Calculator()
+        {
+            string word = Console.ReadLine();
+            int a=int.Parse(Console.ReadLine());
+            int b=int.Parse(Console.ReadLine());
+            int sum = 0;
+            if (word== "add")
+            {
+                Console.WriteLine(   sum = a + b);
+            }
+            else if (word == "multiply")
+            {
+                Console.WriteLine(   sum = a * b);
+            }
+            else if (word== "subtract")
+            {
+                Console.WriteLine(   sum = a - b);
+            }
+            else if (word== "divide")
+            {
+                Console.WriteLine(   sum = a / b);
+            }
+        }
+    }
+}
---
+++ CSHarp Fundamentals/Methods/P05. Orders/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace P05._Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Drinks();
+        }
+        static void Drinks(double coffee=1.50,double water=1.00,double coke=1.40,double snacks=2.00)
+        {
+            string drink=Console.ReadLine();
+            int times = int.Parse(Console.ReadLine());
+            double sum = 0;
+            switch (drink)
+            {
+                case "coffee":
+                    sum =times* coffee;
+                    break;
+                case "water":
+                    sum = times * water;
+                    break;
+                case "coke":
+                    sum = times * coke;
+                    break;
+                case "snacks":
+                    sum = times * snacks;
+                    break;
+                
+            }
+            Console.WriteLine($"{sum:f2}");
+        }
+    }
+}
