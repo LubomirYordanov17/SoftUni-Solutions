f2a6e2c461cbc9a2b23b72b75809ed0b70b7a709
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-09 21:29:38 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Methods/P02. Vowels Count/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+namespace P02._Vowels_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+           int vowelsCount=VowelsCount(word);
+            Console.WriteLine(vowelsCount);
+        }
+        static int VowelsCount(string word)
+        {
+            char[] vowels = new char[] {'a', 'e', 'i', 'o','u' };
+            int vowelsCount =0;
+            foreach (char ch in word.ToLower())
+            {
+                if (vowels.Contains(ch))
+                {
+                    vowelsCount++;
+
+                }
+            }
+            return vowelsCount; 
+        }
+    }
+}
