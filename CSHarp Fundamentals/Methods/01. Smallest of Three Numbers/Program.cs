1215f53d8eb259c6a3d0a88f5e41accbf2e3d043
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-09 21:29:01 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Methods/01. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _01._Smallest_of_Three_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MinNumber();
+            
+        }
+        static void MinNumber()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int minNum = 0;
+            if (a <= b && a <= c)
+            {
+                minNum = a;
+            }
+            if (b <= a && b<=c)
+            {
+                minNum=b; 
+            }
+            if (c <= a && c <= b)
+            {
+                minNum=c;
+            }
+            
+            Console.WriteLine(minNum);
+        }
+    }
+}
