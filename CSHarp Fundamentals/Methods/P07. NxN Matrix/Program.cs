f75d6dc2e067a5856fdae27f1fb5de88dfb4bb73
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-10 10:21:41 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Methods/P07. NxN Matrix/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace P07._NxN_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix();
+        }
+       static void Matrix()
+        {
+            int num=int.Parse(Console.ReadLine());
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = 0; j < num; j++)
+                {
+                    Console.Write($"{num} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
