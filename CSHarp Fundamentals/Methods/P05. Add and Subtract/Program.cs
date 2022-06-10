68724b3daf7536c3a3d76e28373f2b05e7b8fa5b
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-10 10:21:17 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Methods/P05. Add and Subtract/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P05._Add_and_Subtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+            
+            int finalResult=Subtract(firstNum, secondNum, thirdNum);    
+            Console.WriteLine(finalResult);
+        }
+        static int Add(int firstNum,int secondNum)
+        {
+            int result=firstNum + secondNum;
+            return result;
+
+        }
+        static int Subtract(int firstNum, int secondNum,int thirdNum)
+        {
+            int result = Add(firstNum,secondNum)-thirdNum;
+            return result;
+
+        }
+    }
+}
