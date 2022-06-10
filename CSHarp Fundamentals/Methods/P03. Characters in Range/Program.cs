7cd7afeaf9fbdff795ebbab6a2152a02708e2f29
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-10 10:20:47 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Methods/P03. Characters in Range/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace P03._Characters_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintASCII();
+        }
+        static void PrintASCII()
+        {
+            char start=char.Parse(Console.ReadLine());
+            char end=char.Parse(Console.ReadLine());
+            
+            int startInt=(int)start;
+            int endInt=(int)end;
+            if (startInt<endInt)
+            {
+
+                for (int i = startInt + 1; i < endInt; i++)
+                {
+                    Console.Write($"{(char)i} ");
+                }
+            }
+            else
+            {
+                for (int i = endInt + 1; i < startInt; i++)
+                {
+                    Console.Write($"{(char)i} ");
+                }
+            }
+        }
+    }
+}
