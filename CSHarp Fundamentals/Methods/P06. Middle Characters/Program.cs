8c6c700196967d36242a9d2b0b4e8f3690ecf1f3
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-10 10:21:26 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Methods/P06. Middle Characters/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace P06._Middle_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MiddleIndex();
+        }
+        static void MiddleIndex()
+        {
+            string word = Console.ReadLine();
+            string index = "";
+            int half;
+            int half2;
+            if (word.Length % 2 == 0)
+            {
+                half = word.Length / 2;
+                half2 = word.Length / 2 - 1;
+                index += word[half2];
+                index += word[half] ;
+                
+            }
+            else
+            {
+                half = word.Length / 2 ;
+                index += word[half];
+
+            }
+            Console.WriteLine(index);
+
+        }
+    }
+}
