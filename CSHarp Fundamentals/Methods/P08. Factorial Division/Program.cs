0d69315de26cf22f3029c59ed40bb5aaba805232
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-11 22:19:16 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Methods/P08. Factorial Division/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace P08._Factorial_Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int firstNum=int.Parse(Console.ReadLine());
+           int secondNum=int.Parse(Console.ReadLine());
+            double result=FactorialDivision(firstNum, secondNum);
+            Console.WriteLine($"{result:f2}");
+        }
+        static double FactorialDivision(int firstNum,int secondNum)
+        {
+            double sum1 = firstNum;
+            double sum2 = secondNum;
+            double sum = 0;
+            for (int i = firstNum-1;i>=1; i--)
+            {
+                sum1 *= i;
+            }
+            for (int i = secondNum-1; i >=1; i--)
+            {
+                sum2 *= i;
+            }
+            sum = sum1 / sum2;
+            return sum;
+        }
+    }
+}
