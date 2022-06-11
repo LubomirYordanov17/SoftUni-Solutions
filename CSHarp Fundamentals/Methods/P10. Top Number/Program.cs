81e9ad037feaf14ebe2d379dd6c3c15a6d42002d
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-11 22:19:57 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Methods/P10. Top Number/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace P10._Top_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            bool flag = false;
+            int currNum = 0;
+            int sum = 0;
+            for (int i = 17; i <= number; i++)
+            {
+                int realNum = i;
+                while (realNum > 0)
+                {
+                    currNum = realNum % 10;
+                    sum += currNum;
+                    if (currNum % 2 != 0)
+                    {
+                        flag = true;
+                    }
+                    realNum /= 10;
+
+                }
+                if (sum % 8 == 0 && flag == true)
+                {
+                    Console.WriteLine(i);
+                }
+                flag = false;
+                sum = 0;
+            }
+        }
+       
+
+
+    }
+}
