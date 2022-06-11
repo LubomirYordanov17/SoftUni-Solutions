bcf609990df62e4dd435dfaf9a22973dcffa8aec
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-11 22:19:38 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Methods/P09. Palindrome Integers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace P09._Palindrome_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        static void PalindromeIntegers()
+        {
+
+            string a = Console.ReadLine();
+            
+            while (a!="END")
+            {
+                
+
+            }
+
+        }
+
+    }
+}
