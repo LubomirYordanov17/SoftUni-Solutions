abbe900a4a161493175a4291ee5c80900a568248
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-12 14:11:51 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Dictionaries/04. Word Filter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+namespace _04._Word_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[]words=Console.ReadLine().Split().Where(x=>x.Length%2==0).ToArray();
+            foreach (var item in words)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
