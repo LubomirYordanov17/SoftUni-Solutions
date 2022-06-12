8e9da80c75ef6c0c8c41623d69fa8156e99fe65a
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-12 14:10:56 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Dictionaries/01. Count Real Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _01._Count_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers=Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            SortedDictionary<int,int>countNums=new SortedDictionary<int,int>(); 
+            foreach (int number in numbers)
+            {
+                if (countNums.ContainsKey(number))
+                {
+                    countNums[number]+=1;
+                }
+                else
+                {
+                    countNums.Add(number, 1);
+                }
+            }
+            foreach (var item in countNums)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
