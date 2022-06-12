866e6d60e650efc70e4ab3cc336d1f709eb698c5
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-12 14:11:35 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Dictionaries/03. Word Synonyms/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Word_Synonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number=int.Parse(Console.ReadLine());
+            Dictionary<string,List<string>> words = new Dictionary<string,List<string>>();
+            for (int i = 0; i < number; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+                if (words.ContainsKey(word))
+                {
+                    words[word].Add(synonym);
+                }
+                else
+                {
+                    List<string> list = new List<string>();
+                    list.Add(synonym);
+                    words.Add(word, list);
+                }
+            }
+            foreach (var word in words)
+            {
+                Console.WriteLine($"{word.Key} - {string.Join(", ",word.Value)}");
+            }
+        }
+    }
+}
