5a17b7d9a8d69044fd6a1f270a87ae82776d4d5a
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-12 14:11:20 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Dictionaries/02. Odd Occurrences/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Odd_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ').ToArray();
+            Dictionary<string, int> occurencies = new Dictionary<string, int>();
+            foreach (var word in words)
+            {
+                string wordInLower=word.ToLower();  
+                if (occurencies.ContainsKey(wordInLower))
+                {
+                    occurencies[wordInLower] += 1;
+                }
+                else
+                {
+                    occurencies.Add(wordInLower, 1);
+                }
+            }
+            
+            string[]result=occurencies.Where(i=>i.Value%2!=0).Select(i=>i.Key).ToArray();
+            Console.WriteLine(string.Join(" ",result));
+        }
+    }
+}
