70a1ea2c15aa8150a24c912c1cf8890dadea8d43
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-13 11:57:16 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Dictionaries/01. Count Chars in a String/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Count_Chars_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> counter = new Dictionary<char, int>();
+            string text = Console.ReadLine();
+            char[] chars = text.ToCharArray().Where(x=>x!=' ').ToArray();
+            chars.ToList().ForEach(x =>
+            {
+                if (counter.ContainsKey(x))
+                {
+                    counter[x]++;
+                }
+                else
+                {
+                    counter.Add(x, 1);
+                }
+            });
+            foreach (var item in counter)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
