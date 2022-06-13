ea2f565d5a6992ebd751203de2b46ea640a59aac
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-13 11:57:27 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Dictionaries/02. A Miner Task/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string commadString=Console.ReadLine();
+            int commandInt=int.Parse(Console.ReadLine());
+            Dictionary<string, int> resources = new Dictionary<string, int>();
+            while (commadString!="stop")
+            {
+                if (resources.ContainsKey(commadString))
+                {
+                    resources[commadString]+=commandInt;
+
+                }
+                else
+                {
+                resources.Add(commadString, commandInt);
+                }
+                commadString = Console.ReadLine();
+                if (commadString == "stop")
+                {
+                    break;
+                }
+                commandInt = int.Parse(Console.ReadLine());
+            }
+            foreach (var item in resources)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
