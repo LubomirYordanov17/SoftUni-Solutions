0cdcf9d6d15ce1156e2b55aaad550c6793c5f317
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-14 16:46:42 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Dictionaries/04. Orders/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<double>> orders = new Dictionary<string, List<double>>();
+
+            string command = Console.ReadLine();
+            while (command != "buy")
+            {
+                string[] data = command.Split();
+                string name = data[0];
+                double price = double.Parse(data[1]);
+                double quantity = double.Parse(data[2]);
+
+                if (orders.ContainsKey(name))
+                {
+                    orders[name][0] = price;
+                    orders[name][1] += quantity;
+                }
+                else
+                {
+                    List<double> temporary = new List<double>() { price, quantity };
+                    orders.Add(name, temporary);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var item in orders)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value.ElementAt(0) * item.Value.ElementAt(1):f2}");
+            }
+        }
+    }
+}
