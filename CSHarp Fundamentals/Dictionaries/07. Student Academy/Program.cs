1a3a3a55cf37ea73038c0ac6966d6e28eb4601e1
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-15 21:04:10 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Dictionaries/07. Student Academy/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Student_Academy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+            int n=int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < n; i++)
+            {
+                string command = Console.ReadLine();
+                double mark=double.Parse(Console.ReadLine());
+                
+                if (students.ContainsKey(command))
+                {
+                    students[command].Add(mark);
+                }
+                else
+                {
+                    List<double> marks = new List<double>() { mark };
+                    students.Add(command, marks);
+                }
+            }
+            students = students
+               .ToList()
+               .Where(x => x.Value.Average() >= 4.50d)
+               .ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var item in students)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value.Average():f2}");
+            }
+        }
+    }
+}
