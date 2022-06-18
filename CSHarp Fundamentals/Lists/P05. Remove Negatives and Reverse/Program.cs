332cf9337ed9ff12a8af30dc44f66406eb6142d3
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-18 22:08:46 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Lists/P05. Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P05._Remove_Negatives_and_Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<int> positiveNums=new List<int>();
+            List<int> reversedNumbers=new List<int>();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i]>0)
+                {
+                    positiveNums.Add(numbers[i]);
+                }
+                
+            }
+            for (int i = positiveNums.Count-1; i>=0 ; i--)
+            {
+                reversedNumbers.Add(positiveNums[i]);
+            }
+
+            if (reversedNumbers.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine(String.Join(" ", reversedNumbers));
+            }
+        }
+    }
+}
