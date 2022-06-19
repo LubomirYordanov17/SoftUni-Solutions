2142d2a8ab84fd63aeae5e7ba929725d0478e0dd
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-19 22:26:00 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Lists/01. Train/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            int capacity=int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+           
+                while (command!="end")
+             {
+                List<string> firstCom = command.Split(' ').ToList();
+                if (firstCom[0]=="Add")
+                {
+                    int index = int.Parse(firstCom[1]);
+                    wagons.Add(index);
+                    command = Console.ReadLine();
+                    continue;
+                }
+                int number=int.Parse(command);
+                for (int i = 0; i < wagons.Count; i++)
+                {
+                    if ((wagons[i]+number)<=capacity)
+                    {
+                        wagons[i] += number;
+                        i++;
+                        break;
+                    }
+                }
+
+
+               command = Console.ReadLine();
+            }
+            Console.WriteLine(String.Join(" ",wagons));
+        }
+    }
+}
