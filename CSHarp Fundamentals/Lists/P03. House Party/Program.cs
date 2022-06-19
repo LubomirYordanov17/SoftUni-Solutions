bae5c09449da010aa18d3d22f65ac6cdeb0d8a7a
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-19 22:26:28 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Lists/P03. House Party/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03._House_Party
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCommands=int.Parse(Console.ReadLine());
+            List<string> guests=new List<string>();
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string[] command = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                if (command.Length==3)
+                {
+                    if(guests.Contains(command[0]))
+                    {
+                        Console.WriteLine($"{command[0]} is already in the list!");
+                        continue;
+                    }
+                    guests.Add(command[0]);
+                }
+                else if (command.Length==4)
+                {
+                    if (guests.Contains(command[0]))
+                    {
+                        guests.Remove(command[0]);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{command[0]} is not in the list!");
+                    }
+                }
+            }
+            for (int i = 0; i < guests.Count; i++)
+            {
+                Console.WriteLine(guests[i]);
+            }
+        }
+    }
+}
