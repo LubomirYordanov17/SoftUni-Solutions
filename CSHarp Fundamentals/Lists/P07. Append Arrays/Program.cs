0ce82c4c657a5fbbab7e6070e67dee698581699c
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-20 18:25:29 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/Lists/P07. Append Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P07._Append_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> numbers = Console.ReadLine()
+              .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+              .ToList();
+            List<string> numbers2 = new List<string>();
+            List<string> numbers3 = 
+
+
+            //for (int i = numbers.Count-1; i >=0; i--)
+            //{
+            //    numbers2.Add(numbers[i]);
+            //}
+            Console.WriteLine(String.Join(" ",numbers2));
+            
+        }
+    }
+}
