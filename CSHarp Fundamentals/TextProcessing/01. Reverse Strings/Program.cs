a28e5d28477e5e6e10d364a0b29e525c7f1d8623
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-23 20:58:54 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/TextProcessing/01. Reverse Strings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01._Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            while (command!="end")
+            {
+                char[] arr=command.ToCharArray();
+                Array.Reverse(arr);
+                string reversedword=new string(arr);
+
+                Console.WriteLine($"{command} = {reversedword}");
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
