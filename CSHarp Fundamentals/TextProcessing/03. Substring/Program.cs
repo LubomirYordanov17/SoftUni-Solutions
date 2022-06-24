d821ba898ce0115ac43c20576afdfbcd522786ed
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-24 11:07:24 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/TextProcessing/03. Substring/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _03._Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string wordToRemove=Console.ReadLine();
+            string word = Console.ReadLine();
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word.Contains(wordToRemove))
+                {
+                    int index = word.IndexOf(wordToRemove);
+                    word=word.Remove(index,wordToRemove.Length);
+                }
+
+            }
+            Console.WriteLine(word);
+        }
+    }
+}
