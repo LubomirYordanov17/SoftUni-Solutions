817b8be5b694ad5936020fa82f53dfc1946ca58a
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-24 11:07:45 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/TextProcessing/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace _05._Digits__Letters_and_Other
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text=Console.ReadLine();
+            StringBuilder letters=new StringBuilder();
+            StringBuilder digits=new StringBuilder();
+            StringBuilder chars=new StringBuilder();
+            foreach (char item in text)
+            {
+                if (char.IsLetter(item))
+                {
+                    letters.Append(item);
+                }
+                else if (char.IsDigit(item))
+                {
+                    digits.Append(item);
+                }
+                else 
+                {
+                    chars.Append(item);
+                }
+            }
+            Console.WriteLine(letters);
+            Console.WriteLine(digits);
+            Console.WriteLine(chars);
+        }
+    }
+}
