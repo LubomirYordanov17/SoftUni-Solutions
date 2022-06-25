62da5a9945bda1c1470cd470695e1f4e6d7b9c04
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-25 22:28:13 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/TextProcessing/03.Extract File/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _03.Extract_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            int fileNeeded=command.LastIndexOf("\\");
+            string file=command.Substring(fileNeeded+1);
+            string [] fille=file.Split('.');
+            string name=fille[0];
+            string path=fille[1];
+            Console.WriteLine($"File name: {name}");
+            Console.WriteLine($"File extension: {path}");
+        }
+    }
+}
