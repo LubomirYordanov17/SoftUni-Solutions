b45ef6b909a1aeaff865a85a10fe1509276d4b71
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-26 21:32:22 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/TextProcessing/05. Multiply Big Number/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Numerics;
+
+namespace _05._Multiply_Big_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger first = BigInteger.Parse(Console.ReadLine());
+            BigInteger second =BigInteger.Parse(Console.ReadLine());
+            Console.WriteLine(first * second);
+        }
+    }
+}
