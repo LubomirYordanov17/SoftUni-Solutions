c1b398ca00f99f6e026c612475b087eb1c3ca72d
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-27 21:43:05 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/TextProcessing/07. String Explosion/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+namespace _07._String_Explosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input=Console.ReadLine();
+            StringBuilder output=new StringBuilder();
+            int bombpower=0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currCh = input[i];
+                if (currCh == '>')
+                {
+                    int currentBombPower = Get(input[i+1]);
+                    output.Append(currCh);
+                    bombpower+=currentBombPower;
+                }
+                else
+                {
+                    if (bombpower>0)
+                    {
+                        bombpower--;
+                    }
+                    else
+                    {
+                        output.Append(currCh);
+                    }
+                }
+            }
+            Console.WriteLine(output);
+
+        }
+        static int Get(char ch)
+        {
+            return (int)ch - 48;
+        }
+    }
+}
