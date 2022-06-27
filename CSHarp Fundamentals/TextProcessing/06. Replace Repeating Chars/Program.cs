0414c8a3806836795d976d123e2c53b608263357
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-06-27 21:42:52 +0300

    Add files via upload

---
+++ CSHarp Fundamentals/TextProcessing/06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _06._Replace_Repeating_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string result = string.Empty;
+
+            for (int i = 0; i < input.Length-1; i++)
+            {
+                if (input[i]!=input[i+1])
+                {
+                    result += input[i];
+                }
+                
+            }
+            result += input[input.Length - 1];
+            Console.WriteLine(result);
+        }
+    }
+}
