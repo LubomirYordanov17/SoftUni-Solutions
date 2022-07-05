266343b2f24fdb194715626d2b8a8025d0bebf58
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-07-05 16:45:31 +0300

    Add files via upload

---
+++ CSharp Advanced/Multidimensional Arrays/3. Primary Diagonal/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Primary_Diagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n,n];
+            int sumOfDiagonal = 0;
+            for (int row = 0; row < n; row++)
+            {
+                int[] rowOfMatrix = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = rowOfMatrix[col];
+                    if (row==col)
+                    {
+                        sumOfDiagonal += rowOfMatrix[col];
+                    }
+                }
+            }
+            Console.WriteLine(sumOfDiagonal);
+        }
+    }
+}
