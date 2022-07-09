78fdb2622c6370848352f9a968255e6a118deaac
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-07-09 15:18:08 +0300

    Add files via upload

---
+++ CSharp Advanced/Multidimensional Arrays/1. Diagonal Difference/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Diagonal_Difference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            for (int row = 0; row < n; row++)
+            {
+                int[] rowOfMatrix = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                for (int col = 0; col < n; col++)
+                {
+                    
+                    matrix[row, col] = rowOfMatrix[col];
+                }
+            }
+            int sumOfFirstDiagonal = 0;
+            int sumOfSecondDiagonal = 0;
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (row==col)
+                    {
+                        sumOfFirstDiagonal+=matrix[row, col];
+                    }
+                    if (row+col==n-1)
+                    {
+                        sumOfSecondDiagonal += matrix[row, col];
+                    }
+                }
+            }
+            Console.WriteLine(Math.Abs(sumOfFirstDiagonal-sumOfSecondDiagonal));
+        }
+    }
+}
