73c99918225bbf007ae14b85c4d98abcb45b0e1c
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-07-25 13:44:17 +0300

    Add files via upload

---
+++ CSharp Advanced/SetsAndDictionaries/05. Count Symbols/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _05._Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           SortedDictionary<char,int> keyValuePairs = new SortedDictionary<char,int>();
+            string input = Console.ReadLine();
+            char[] data =input.ToCharArray();
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (keyValuePairs.ContainsKey(data[i]))
+                {
+                    keyValuePairs[data[i]]++;
+                }
+                else
+                {
+                    keyValuePairs.Add(data[i], 1);
+                }
+            }
+            foreach (var item in keyValuePairs)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+            
+        }
+    }
+}
