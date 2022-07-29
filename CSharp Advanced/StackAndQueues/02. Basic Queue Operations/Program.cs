0016800ae75d441927899ff3b84cf07ac7ff5c4e
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-07-29 15:44:09 +0300

    Add files via upload

---
+++ CSharp Advanced/StackAndQueues/02. Basic Queue Operations/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Basic_Queue_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersInput = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int n = numbersInput[0];
+            int s = numbersInput[1];
+            int x = numbersInput[2];
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Queue<int> queue = new Queue<int>();
+            for (int i = 0; i < n; i++)
+            {
+                queue.Enqueue(numbers[i]);
+            }
+            for (int i = 0; i < s; i++)
+            {
+                queue.Dequeue();
+            }
+            if (queue.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (queue.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(queue.Min());
+            }
+        }
+    }
+}
