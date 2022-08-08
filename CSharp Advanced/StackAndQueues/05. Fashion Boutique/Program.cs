3a730e9585ea430c0f9d2b369aa5931105f7554f
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-08-08 19:49:59 +0300

    Add files via upload

---
+++ CSharp Advanced/StackAndQueues/05. Fashion Boutique/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Fashion_Boutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothesInBox = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> clothes = new Stack<int>(clothesInBox);
+
+            int capacityOfRack = int.Parse(Console.ReadLine());
+            int currentRackCapacity = capacityOfRack;
+            int racksCount = 1;
+
+            while (clothes.Any())
+            {
+                int cloth = clothes.Pop();
+                currentRackCapacity -= cloth;
+
+                if (currentRackCapacity < 0)
+                {
+                    racksCount += 1;
+                    currentRackCapacity = capacityOfRack - cloth;
+                }
+            }
+            Console.WriteLine(racksCount);
+        }
+    }
+}
