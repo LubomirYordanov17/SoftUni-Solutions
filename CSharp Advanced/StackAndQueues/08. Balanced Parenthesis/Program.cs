d0b118215ce7ab70700e6bcda837590f0ee1482d
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-08-12 15:03:19 +0300

    Add files via upload

---
+++ CSharp Advanced/StackAndQueues/08. Balanced Parenthesis/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._Balanced_Parenthesis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+            Stack<char> parentheses = new Stack<char>();
+            bool balanced = false;
+            char ch = expression[0];
+           
+            foreach (char item in expression)
+            {
+                if (item == '(' || item == '{' || item == '[')
+                {
+                    parentheses.Push(item);
+                }
+                else if (item == ')' || item == '}' || item == ']')
+                {
+                    if (parentheses.Count!=0)
+                    {
+
+
+                        string brackets = parentheses.Peek().ToString() + item;
+
+                        if ((brackets != "[]") && (brackets != "{}") && (brackets != "()"))
+                        {
+                            balanced = false;
+                            break;
+                        }
+                        else
+                        {
+                            balanced = true;
+                            parentheses.Pop();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+                }
+            }
+
+            if (balanced)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
