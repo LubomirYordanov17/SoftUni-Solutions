87893558083d6775101e20e82d2ad81e564cb6a2
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-08-13 14:09:05 +0300

    Add files via upload

---
+++ CSharp Advanced/StackAndQueues/09. Simple Text Editor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09._Simple_Text_Editor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder text = new StringBuilder();
+            int n = int.Parse(Console.ReadLine());
+            Stack<string>history=new Stack<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split(" ");
+                if (command[0] == "1")
+                {
+                    history.Push(text.ToString());
+                    text.Append(command[1]);
+                }
+                else if (command[0] == "2")
+                {
+                    history.Push(text.ToString());
+                    text.Remove(text.Length-int.Parse(command[1]), int.Parse(command[1]));
+                }
+                else if (command[0] == "3")
+                {
+                    Console.WriteLine(text[int.Parse(command[1])-1]);
+                }
+                else if (command[0] == "4")
+                {
+                    text=new StringBuilder(history.Pop());
+                }
+            }
+
+        }
+    }
+}
