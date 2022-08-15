8903c54a61c836abb2db34c0f3e27d6fedc9ffc4
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-08-15 22:23:32 +0300

    Add files via upload

---
+++ CSharp Advanced/Defining Classes/Car Salesman/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarSalesman
+{
+    public class Car
+    {
+        public string Model { get; set; }
+
+        public Engine Engine { get; set; }
+
+        public string Weight { get; set; }
+
+        public string Color { get; set; }
+
+        public Car(string model, Engine engine, string weight, string color)
+        {
+            this.Model = model;
+            this.Engine = engine;
+            this.Weight = weight;
+            this.Color = color;
+        }
+
+        public override string ToString()
+        {
+            return
+                $"{this.Model}:" + Environment.NewLine +
+                $"  {this.Engine.Model}:" + Environment.NewLine +
+                $"    Power: {this.Engine.Power}" + Environment.NewLine +
+                $"    Displacement: {this.Engine.Displacement}" + Environment.NewLine +
+                $"    Efficiency: {this.Engine.Efficiency}" + Environment.NewLine +
+                $"  Weight: {this.Weight}" + Environment.NewLine +
+                $"  Color: {this.Color}";
+        }
+    }
+}
---
+++ CSharp Advanced/Defining Classes/Car Salesman/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace CarSalesman
+{
+    public class Program
+    {
+        
+            static void Main(string[] args)
+            {
+                int n = int.Parse(Console.ReadLine());
+
+                List<Engine> engineList = new List<Engine>();
+                List<Car> cars = new List<Car>();
+
+                for (int i = 0; i < n; i++)
+                {
+                    string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                    string model = input[0];
+                    string power = input[1];
+
+                    string displacement = "n/a";
+                    string efficiency = "n/a";
+
+                    if (input.Length > 2)
+                    {
+                        if (char.IsDigit(input[2][0]))
+                        {
+                            displacement = input[2];
+                        }
+                        else
+                        {
+                            efficiency = input[2];
+                        }
+                    }
+
+                    if (input.Length > 3)
+                    {
+                        if (char.IsDigit(input[3][0]))
+                        {
+                            displacement = input[3];
+                        }
+                        else
+                        {
+                            efficiency = input[3];
+                        }
+                    }
+
+                    Engine currEngine = new Engine(model, power, displacement, efficiency);
+                    engineList.Add(currEngine);
+                }
+
+                int m = int.Parse(Console.ReadLine());
+
+                for (int i = 0; i < m; i++)
+                {
+                    string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                    string model = input[0];
+                    string engine = input[1];
+
+                    string weight = "n/a";
+                    string color = "n/a";
+
+                    if (input.Length > 2)
+                    {
+                        if (char.IsDigit(input[2][0]))
+                        {
+                            weight = input[2];
+                        }
+                        else
+                        {
+                            color = input[2];
+                        }
+                    }
+
+                    if (input.Length > 3)
+                    {
+                        if (char.IsDigit(input[3][0]))
+                        {
+                            weight = input[3];
+                        }
+                        else
+                        {
+                            color = input[3];
+                        }
+                    }
+
+                    Engine engineForCar = engineList.FirstOrDefault(e => e.Model == engine);
+                    Car currentCar = new Car(model, engineForCar, weight, color);
+                    cars.Add(currentCar);
+                }
+
+                foreach (var car in cars)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+        
+    }
+}
