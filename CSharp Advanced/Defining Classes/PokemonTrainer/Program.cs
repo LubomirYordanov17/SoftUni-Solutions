5b049775b5037847bff0bd72a8183f97d95d6e33
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-08-17 15:10:48 +0300

    Add files via upload

---
+++ CSharp Advanced/Defining Classes/PokemonTrainer/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace PokemonTrainer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            List<Trainer> trainers = new List<Trainer>();
+
+            while (command.ToLower() != "tournament")
+            {
+                string[] information = command.Split();
+                string trainerName = information[0];
+                string pokemonName = information[1];
+                string pokemonElement = information[2];
+                int pokemonHealth = int.Parse(information[3]);
+
+                Pokemon pokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+
+                if (trainers.Any(t => t.Name == trainerName))
+                {
+                    Trainer trainer = trainers.FirstOrDefault(t => t.Name == trainerName);
+                    trainer.PokemonCollection.Add(pokemon);
+                }
+                else
+                {
+                    Trainer trainer = new Trainer(trainerName, 0, new List<Pokemon>() { pokemon });
+                    trainers.Add(trainer);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            command = Console.ReadLine();
+            while (command.ToLower() != "end")
+            {
+                for (int trainer = 0; trainer < trainers.Count; trainer++)
+                {
+                    Trainer currentTrainer = trainers[trainer];
+                    if (currentTrainer.PokemonCollection.Any(p => p.Element == command))
+                    {
+                        currentTrainer.NumberOfBadges += 1;
+                    }
+                    else
+                    {
+                        for (int pokemon = 0; pokemon < currentTrainer.PokemonCollection.Count; pokemon++)
+                        {
+                            Pokemon currentPokemon = currentTrainer.PokemonCollection[pokemon];
+                            currentPokemon.Health -= 10;
+
+                            if (currentPokemon.Health <= 0)
+                            {
+                                currentTrainer.PokemonCollection.Remove(currentPokemon);
+                            }
+                        }
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            trainers = trainers.OrderByDescending(x => x.NumberOfBadges).ToList();
+
+            foreach (Trainer trainer in trainers)
+            {
+                Console.WriteLine($"{trainer.Name} {trainer.NumberOfBadges} {trainer.PokemonCollection.Count}");
+            }
+        }
+    
+    }
+}
