61e0a0cfacaaba456cef54312ca06b6c330bf203
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-08-24 21:49:52 +0300

    Add files via upload

---
+++ CSharp OOP/Inheritance/Restaurant/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Restaurant
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Cake cake = new Cake("asd");
+            Console.WriteLine(cake.Price);
+            Console.WriteLine(cake.Calories);
+        }
+    }
+}
