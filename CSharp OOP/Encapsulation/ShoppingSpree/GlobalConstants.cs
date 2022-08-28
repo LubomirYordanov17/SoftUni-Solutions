ec724fcd864a18cfff17c6284260d818e943bc8f
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-08-28 14:14:52 +0300

    Add files via upload

---
+++ CSharp OOP/Encapsulation/ShoppingSpree/GlobalConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    public class GlobalConstants
+    {
+        
+            public static string InvalidNameExceptionMessage = "Name cannot be empty";
+            public static string InvalidMoneyExceptionMessage = "Money cannot be negative";
+            public static string InsufficientMoneyExceptionMessage = "{0} can't afford {1}";
+            public const decimal CostMinValue = 0m;
+       
+    }
+}
---
+++ CSharp OOP/Encapsulation/ShoppingSpree/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ShoppingSpree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Engine engine = new Engine();
+                engine.Run();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
