229d3ca1926c5b52aa51016c99fcb3be973f5cae
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2022-09-09 11:21:20 +0300

    Add files via upload

---
+++ CSharp OOP/Interfaces and Abstraction/03. Telephony/ICalling.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public interface ICalling
+    {
+        public void Call(string number);
+        
+    }
+}
---
+++ CSharp OOP/Interfaces and Abstraction/03. Telephony/StartUp.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+
+namespace PersonInfo
+{
+    public class StartUp
+    {
+        
+            static void Main(string[] args)
+            {
+                string[] phonesToCall = Console.ReadLine().Split(' ');
+                string[] sitesToVisit = Console.ReadLine().Split(' ');
+
+                Smartphone smartphone = new Smartphone();
+                StationaryPhone stationaryPhone = new StationaryPhone();
+
+                CallPhoneNumbers(phonesToCall, smartphone, stationaryPhone);
+                BrowseWebsites(sitesToVisit, smartphone);
+            }
+
+            private static void BrowseWebsites(string[] sitesToVisit, Smartphone smarthphone)
+            {
+                foreach (string url in sitesToVisit)
+                {
+                    bool isNotValid = url.Any(ch => char.IsDigit(ch));
+
+                    if (isNotValid)
+                    {
+                        Console.WriteLine("Invalid URL!");
+                    }
+                    else
+                    {
+                        smarthphone.Browse(url);
+                    }
+                }
+            }
+
+            private static void CallPhoneNumbers(string[] phonesToCall, Smartphone smarthphone, StationaryPhone stationaryPhone)
+            {
+                foreach (string number in phonesToCall)
+                {
+                    bool isValid = number.All(ch => char.IsDigit(ch));
+
+                    if (isValid)
+                    {
+                        if (number.Length == 10)
+                        {
+                            smarthphone.Call(number);
+                        }
+                        else if (number.Length == 7)
+                        {
+                            stationaryPhone.Call(number);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid number!");
+                    }
+                }
+            
+        }
+    }
+}
